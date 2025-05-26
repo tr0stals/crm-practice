e31c168451542794ec527e926af1f6e619b6e2f4
pavelbekarev <bekarev.ps@gmail.com>
2025-05-26 13:36:05 +0500

    feat: добавил универсальную кнопку Button, иконки.

---
+++ frontend/src/shared/ui/Button/ui/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="printText" class="button" :class="buttonColor">
+    <component v-if="image" :is="image" class="button__icon" />
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import "../style.scss";
+
+/*
+ * text - текст кнопки,
+ * image - иконка кнопки (необяз.),
+ * buttonColor - цвет фона кнопки (по умол. серый, необяз.)
+ */
+const props = defineProps<{
+  text: string;
+  image?: any;
+  buttonColor?: string;
+}>();
+
+const printText = () => {
+  console.debug("!!!", props.text);
+};
+</script>
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,17 +1,11 @@
-<!-- 
-  Временная страница. Вместо нее перебрасывать пользователя на главную станицу
--->
-<template>
-  <div>
-    <h1>Добро пожаловать в Dashboard!</h1>
-    <p>Вы успешно вошли в систему.</p>
-    <button @click="logout">Выйти</button>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { useAuthStore } from "@/shared/store/auth.store";
 import { useRouter } from "vue-router";
+import Button from "@/shared/ui/Button/ui/Button.vue";
+import PlusIcon from "@/shared/ui/PlusIcon/ui/PlusIcon.vue";
+import EditIcon from "@/shared/ui/EditIcon/ui/EditIcon.vue";
+import DeleteIcon from "@/shared/ui/DeleteIcon/ui/DeleteIcon.vue";
+import RefreshIcon from "@/shared/ui/RefreshIcon/ui/RefreshIcon.vue";
 
 const authStore = useAuthStore();
 const router = useRouter();
@@ -21,3 +15,22 @@ const logout = () => {
   router.push("/login");
 };
 </script>
+
+<!-- 
+  Временная страница. Вместо нее перебрасывать пользователя на главную станицу
+-->
+<template>
+  <div>
+    <h1>Добро пожаловать в Dashboard!</h1>
+    <p>Вы успешно вошли в систему.</p>
+    <button @click="logout">Выйти</button>
+  </div>
+
+  <!-- Пример использования -->
+  <div style="display: flex; flex-direction: row">
+    <Button text="Добавить" :image="PlusIcon" buttonColor="button__buttonAdd" />
+    <Button text="Редактировать" :image="EditIcon" />
+    <Button text="удалить" :image="DeleteIcon" />
+    <Button text="обновить" :image="RefreshIcon" />
+  </div>
+</template>
