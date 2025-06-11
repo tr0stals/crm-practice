2ac1855b9e850f616126d27efa63ede0087dfd2e
SolitudeAlways <Mihuil.chugunov@gmail.com>
2025-06-11 20:22:00 +0500

    fix: убраны конфликтные метки и оставлен актуальный контроллер employee

--- backend/src/employees/employees.controller.ts
+++ backend/src/employees/employees.controller.ts
@@ -1,17 +1,12 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
-<<<<<<< HEAD
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
-=======
-import { EmployeesDTO } from './dto/EmployeesDTO';
->>>>>>> main
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
-<<<<<<< HEAD
   @Post()
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
@@ -35,25 +30,5 @@ export class EmployeesController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.employeesService.remove(+id);
-=======
-  @Post('create')
-  async create(@Body() data: EmployeesDTO) {
-    return await this.employeesService.create(data);
   }
-
-  @Get('get')
-  async getAll() {
-    return await this.employeesService.getAll();
-  }
-
-  @Patch('update/:id')
-  async update(@Param('id') id: number, @Body() data: EmployeesDTO) {
-    return await this.employeesService.update(id, data);
-  }
-
-  @Delete('delete/:id')
-  async delete(@Param('id') id: number) {
-    return await this.employeesService.delete(id);
->>>>>>> main
-  }
-} 
+}
