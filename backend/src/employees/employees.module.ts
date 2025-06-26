d4a8bc06facab136c38bff4fa4bbafa8c4477d25
96ix9ine <sergstar2012@mail.ru>
2025-06-26 12:22:50 +0500

    fix employee

--- backend/src/employees/employees.module.ts
+++ backend/src/employees/employees.module.ts
@@ -7,6 +7,7 @@ import { Peoples } from 'src/peoples/peoples.entity';
 import { Professions } from 'src/professions/professions.entity';
 import { User } from 'src/user/user.entity';
 import { EmployeesProfessions } from 'src/employees-professions/employees-professions.entity';
+import { EmployeeDepartmentsModule } from 'src/employee-departments/employee-departments.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { EmployeesProfessions } from 'src/employees-professions/employees-profes
       User,
       EmployeesProfessions,
     ]),
+    EmployeeDepartmentsModule
   ],
   providers: [EmployeesService],
   controllers: [EmployeesController],
