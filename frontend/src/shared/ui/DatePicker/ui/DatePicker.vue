1453823b6c4fbb7d6f3f568887759f0287e6fe2b
pavelbekarev <bekarev.ps@gmail.com>
2025-10-29 10:47:15 +0500

    refactor: увеличил размер иконок

--- frontend/src/shared/ui/DatePicker/ui/DatePicker.vue
+++ frontend/src/shared/ui/DatePicker/ui/DatePicker.vue
@@ -2,6 +2,7 @@
 import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
 import { computed, onMounted } from "vue";
+import "../style.scss";
 
 const props = defineProps<{
   modelValue: Date | string | null;
@@ -26,6 +27,7 @@ const emit = defineEmits<{
 </script>
 <template>
   <VueDatePicker
+    class="datePicker"
     v-model="value"
     :id="id"
     :name="name"
@@ -34,5 +36,20 @@ const emit = defineEmits<{
     :hide-input-icon="props.config?.hideInputIcon"
     model-type="yyyy-MM-dd"
     auto-apply
-  />
+    :clearable="true"
+  >
+    <template #clear-icon="{clear}" ">
+      <svg
+        @click="clear"
+        class="datePicker__closeIcon"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        <path
+          d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19
+                 12 13.41 17.59 19 19 17.59 13.41 12z"
+        />
+      </svg>
+    </template>
+  </VueDatePicker>
 </template>
